ff95c744190995fd798c6132ca08ec9c380588bc
qhle1981 <qhle1981@gmail.com>
2025-04-09 10:43:45 +0700

    Bootstrap 5

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+
+
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(morgan('combined'));
+app.use(express.static('src\\public'));
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', 'src\\resources\\views');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
